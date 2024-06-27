f0b4bc214a48f44947317c06f9a0fc7065f4c8bd
Mariia Rumiantseva <mrum98@gmail.com>
2024-06-27 11:07:25 +0200

    Remove Pods directory from repository

---
+++ ActioID/Services/UserService.swift
@@ -0,0 +1,8 @@
+//
+//  UserService.swift
+//  ActioID
+//
+//  Created by Mariia Tsymberh on 26.06.24.
+//
+
+import Foundation
--- Pods/Target Support Files/Pods-ActioIDUITests/Pods-ActioIDUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ActioIDUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ActioIDUITests
-@end
